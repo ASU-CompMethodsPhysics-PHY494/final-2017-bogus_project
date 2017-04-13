de51be85b5c3322744e48a4b08f2d5ff78c7592c
Brian Pickens <brian.pickens@yahoo.com>
2017-04-13 00:33:24 -0700

    [yt] Added 3D frame rendering w/ bug. Redesigning

--- Work/visualization.py
+++ Work/visualization.py
@@ -10,70 +10,11 @@ import numpy as np
 import yt
 
 
-################################################################################
-# Testing area - will be deleted later
-resolution = 32
-bboxSize = 1.5
-
-testArray = np.zeros([resolution, resolution, resolution])
-testArray[1:-1, 1:-1, 1:-1] = 0.1
-testArray[6:-6, 6:-6, 6:-6] = 0
-testArray[12:-12, 12:-12, 12:-12] = 0.5
-testArray[14:-14, 14:-14, 14:-14] = 0.9
-
-#testArray[:, :, :] = 
-#testArray[:, :, :] = testArray[:, :, :] * np.cos(
-
-testData = dict(density = (testArray, "g/cm**3"))
-testbbox = np.array([[-bboxSize, bboxSize], [-bboxSize, bboxSize], [-bboxSize, bboxSize]])
-ds = yt.load_uniform_grid(testData, testArray.shape, length_unit = "dimensionless", bbox = testbbox, nprocs = resolution)
-
-sc = yt.create_scene(ds)
-#im, sc = yt.volume_render(ds, 'density', fname='test_frame')
-
-# From yt tutorial:
-# Get a reference to the VolumeSource associated with this scene
-# It is the first source associated with the scene, so we can refer to it
-# using index 0.
-source = sc[0]
-
-# Colormap boundaries (min, max) (don't set any part to 0, it hates 0)
-bounds = (0.01,1)
-tf = yt.ColorTransferFunction(np.log10(bounds))
-tf.add_layers(10, colormap='arbre')
-
-source.tfh.tf = tf
-source.tfh.bounds = bounds
-
-#Our probability data is going to be low resolution, so this will prevent artifacts created as a consequence of that 
-source.set_use_ghost_zones(True)
-
-cam = sc.camera
-
-#cam.zoom(0.4)
-#sc.camera.set_lens('plane-parallel')
-cam.set_lens('perspective')
-cam.set_position([1, 1, 1])
-#sc.camera.set_position(ds.domain_left_edge - 1.5)
-
-# The width determines the opening angle
-#cam.set_width(ds.domain_width)
-frame = 0
-
-for _ in cam.iter_rotate(2 * np.pi, 30, rot_vector = [1, 0, 0], rot_center = ds.domain_center):
-    #sc.render()
-    sc.save('Frames/test_frame{}'.format(frame), sigma_clip = 4)
-    frame += 1
-
-#sc.save('test_frame', sigma_clip = 4)
-################################################################################
-
-
 
 ################################
-### Begin createFrame2D()
+### Begin createFrames2D()
 ################################
-def createFrame2D():
+def createFrames2D():
     '''
     description
     '''
@@ -83,13 +24,58 @@ def createFrame2D():
     
 
 ################################
-### Begin createFrame3D()
+### Begin createFrames3D()
 ################################
-def createFrame3D():
+def createFrames3D(dataSet, totalFrames = 10, rotation = 0):
     '''
-    description
+    Generates animation frames for a 3D pi/2 rotation based on the data set inputted.
+    
+    ----- Arguments -----
+    dataSet: 3D cubic numpy array.
+    totalFrames: Number of frames. More frames means smoother animation. Recommend 24 per second.
+    rotation: gradual rotation occurs around the data set across all frames up until this point.
+    
+    ----- Returns -----
+    Returns nothing. Frames generated are stored in local Frames/3D directory.
     '''
-    return None
+    
+    # 
+    resolution = int(len(dataSet[0, 0, 0]))
+    bboxSize = 1.5
+    boundingBox = np.array([[-bboxSize, bboxSize], [-bboxSize, bboxSize], [-bboxSize, bboxSize]])
+    bounds = (0.01,1)
+    
+    for index in range(totalFrames):
+        frameData = dict(density = (dataSet[index], "g/cm**3"))
+        ds = yt.load_uniform_grid(frameData, dataSet[index].shape, length_unit = "dimensionless", bbox = boundingBox, nprocs = resolution)
+        sc = yt.create_scene(ds)
+
+        # These values don't need to be calculated each step
+        if index == 0:
+            dataSetOrigin = ds.domain_center
+            # Throws a log error, ignore it
+            tf = yt.ColorTransferFunction(np.log10(bounds))
+            tf.add_layers(10, colormap='Blues')    
+
+        source = sc[0]
+        source.tfh.tf = tf
+        source.tfh.bounds = bounds
+        # Our probability data might be low resolution, so this will prevent artifacts created as a consequence of that 
+        source.set_use_ghost_zones(True)
+
+        cam = sc.camera
+        #cam.resolution = 1024
+        cam.set_lens('perspective')
+        cam.north_vector = [0, 0, 1]
+        cam.set_position([1.5, 1.5, 0.75])
+        
+        cam.rotate((index / totalFrames) * rotation, rot_vector = [0, 0, 1], rot_center = dataSetOrigin)
+        #cam.yaw((index / totalFrames) * rotation, [0, 0, 1])
+        
+        sc.save('Frames/3D/test_frame{}'.format(index), sigma_clip = 4)
+        print('-------')
+        
+    return
 ################################
 
 
@@ -97,9 +83,27 @@ def createFrame3D():
 ################################
 ### Begin animateFrames()
 ################################
-def animateFrames():
+def animateFrames(targetDirectory, render3D = True):
     '''
     description
     '''
     return None
 ################################
+
+
+
+################################################################################
+# Testing area - will be deleted later
+resolution = 32
+bboxSize = 1.5
+
+testArray = np.zeros([25, resolution, resolution, resolution])
+testArray[:, 1:-1, 1:-1, 1:-1] = 0.1
+testArray[:, 6:-6, 6:-6, 6:-6] = 0
+testArray[:, 12:-12, 12:-12, 12:-12] = 0.5
+testArray[:, 14:-14, 14:-14, 14:-14] = 0.9
+
+#testArray[1, 12:-12, 12:-12, 12:-12] = 0
+
+createFrames3D(testArray, 7, (0.5 * np.pi))
+################################################################################
