3b4d1d73b3c99065e8955b481523c79637a96274
Nate Chrisman <nachrism@asu.edu>
2017-04-14 22:05:19 -0700

    added function to calculate Gaussian

--- Work/wavepacket.py
+++ Work/wavepacket.py
@@ -6,8 +6,10 @@ hbar = 6.58212e-16 #eV-s
 
 #initial conditions
 
+def Gaussian(x, sigma, t):
+    return np.exp(-(x-t)**2/(2*sigma**2))
 
-def schrodinger_1D(x, t):
+def schrodinger_1D(x, t, v):
     """
     Solves the time-dependent Schrodinger equation in 1D
 
@@ -19,6 +21,9 @@ def schrodinger_1D(x, t):
 
     t : array
         one-dimensional array of time-steps for calculation
+
+    v : float
+        potential step
 
     """
     return None
