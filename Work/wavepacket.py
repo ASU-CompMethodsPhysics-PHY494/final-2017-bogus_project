6a9a31435b9e67409c74f613276ffeda8cf87bef
Nate Chrisman <nachrism@asu.edu>
2017-04-14 21:51:44 -0700

    added simple start file. nothing of great significance

---
+++ Work/wavepacket.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mass_electron = 9.109383e-31 #kg
+hbar = 6.58212e-16 #eV-s
+
+#initial conditions
+
+
+def schrodinger_1D(x, t):
+    """
+    Solves the time-dependent Schrodinger equation in 1D
+
+    Arguments
+    _________
+
+    x : array
+        one-dimensional array for position of the Particle
+
+    t : array
+        one-dimensional array of time-steps for calculation
+
+    """
+    return None
